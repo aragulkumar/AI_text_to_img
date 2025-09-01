385a9e3971a9520830d4057497de6e3fea9df49c
aragulkumar <aragulkumar11@gmail.com>
2025-09-01 23:01:26 +0530

    updated some of views.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class PromptHistory(models.Model):
+    prompt = models.TextField()
+    image = models.ImageField(upload_to='generated/')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+import requests
+from rest_framework.decorators import api_view
+from rest_framework.response import response
+from django.conf import settings
+
+@api_view(['POST'])
+
+def generate_image(request):
+    prompt =  request.data.get("prompt")
+    headers = {"Authorization":f"Bearer {settings.HF_API_KEY}"}
+    payload = {"input": prompt}
+    response = response.post(
+        "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4",
+        headers=headers,
+        json=payload
+    )
+    return response(response.json())
 
 # Create your views here.
