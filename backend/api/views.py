4e8e889d614dacd49dcd87a585eb7a7016b5a657
aragulkumar <aragulkumar11@gmail.com>
2025-09-04 16:46:23 +0530

    changed some backend prgogram

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,20 +1,25 @@
-from django.shortcuts import render
 import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
 
 @api_view(['POST'])
-
 def generate_image(request):
-    prompt =  request.data.get("prompt")
-    headers = {"Authorization":f"Bearer {settings.HF_API_KEY}"}
-    payload = {"input": prompt}
-    response = response.post(
-        "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4",
-        headers=headers,
-        json=payload
-    )
-    return Response(response.json())
+    prompt = request.data.get("prompt")
+    headers = {"Authorization": f"Bearer {settings.HF_API_KEY}"}
+    payload = {"inputs": prompt}
 
-# Create your views here.
+    try:
+        res = requests.post(
+            "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4",
+            headers=headers,
+            json=payload
+        )
+
+        if res.status_code != 200:
+            return Response({"error": res.text}, status=res.status_code)
+
+        return Response(res.json())
+
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
+HF_API_KEY = os.getenv("HF_API_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,4 +130,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
